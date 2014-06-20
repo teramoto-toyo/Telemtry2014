4770d59c22305feb6ab62e531308736702241552
silverdog-jp <silverdog@fukuda-mba.local>
2014-06-20 19:19:58 +0900

    セクションデータ転送実装中

--- relay_db.php
+++ relay_db.php
@@ -12,13 +12,21 @@ Data Relaying...<br />
 	if (!($cn = mysql_pconnect($sql_server, $sql_user, $sql_pw))) { echo "db connect error"; die; }
 	if (!(mysql_select_db($sql_db))) { echo "db connect error"; die; }
 	
+	//最新のtelemetry data 取得
 	$sql = "SELECT * FROM nur2014_telemetry ORDER BY id DESC  LIMIT 1";
-	
 	if (!($rs = mysql_query($sql))) { echo "db query error"; die; }
 	mb_http_output( 'UTF-8' );
+	$data1 = mysql_fetch_array( $rs, MYSQL_BOTH );
+	
+//	//最新のセクションデータ取得
+//	$section_sql = "SELECT a.* FROM nur2014_section a WHERE a.id = (SELECT MAX(b.id) FROM nur2014_section b WHERE b.section_index = a.section_index) ORDER BY a.section_index ASC";
+//	if (!($rs = mysql_query($section_sql))) { echo "db query error"; die; }
+//	mb_http_output( 'UTF-8' );
+//	$data2 = mysql_fetch_array( $rs, MYSQL_BOTH );
 	
 	$url = 'http://66.147.244.133/~sdogjp/nur2014/relay_test.php';
-	$data = mysql_fetch_array( $rs, MYSQL_BOTH );
+//	$data = array($data1, $data2);
+	$data = $data1;
 	$options = array('http' => array(
 	    'method' => 'POST',
 	    'content' => http_build_query($data),
